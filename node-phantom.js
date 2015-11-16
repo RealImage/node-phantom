c8f2dc66f054baa2ffa7085fd2977ac196718bdd
anug <anug@anugs-MacBook-Air.local>
2015-11-16 10:03:50 +0530

    Changed window.location.hostname to window.location.host

--- node-phantom.js
+++ node-phantom.js
@@ -43,21 +43,21 @@ module.exports={
 				callback(hasErrors,phantom);
 			},100);
 		}
-		
+
 		var server=http.createServer(function(request,response){
 			response.writeHead(200,{"Content-Type": "text/html"});
 			response.end('<html><head><script src="/socket.io/socket.io.js" type="text/javascript"></script><script type="text/javascript">\n\
 				window.onload=function(){\n\
-					var socket = new io.connect("http://" + window.location.hostname);\n\
+					var socket = new io.connect("http://" + window.location.host);\n\
 					socket.on("cmd", function(msg){\n\
 						alert(msg);\n\
 					});\n\
 					window.socket = socket;\n\
 				};\n\
 			</script></head><body></body></html>');
-		}).listen(function(){			
+		}).listen(function(){
 			var io=socketio.listen(server,{'log level':1});
-	
+
 			var port=server.address().port;
 			spawnPhantom(port,function(err,phantom){
 				if(err){
@@ -72,11 +72,11 @@ module.exports={
 					args.splice(1,0,cmdid);
 //					console.log('requesting:'+args);
 					socket.emit('cmd',JSON.stringify(args));
-	
+
 					cmds[cmdid]={cb:callback};
 					cmdid++;
 				}
-			
+
 				io.sockets.on('connection',function(socket){
 					socket.on('res',function(response){
 //						console.log(response);
@@ -208,17 +208,17 @@ module.exports={
 						},
 						_phantom: phantom
 					};
-				
+
 					callback(null,proxy);
 				});
-	
+
 				// An exit event listener that is registered AFTER the phantomjs process
 				// is successfully created.
 				var prematureExitHandler=function(code,signal){
 					console.warn('phantom crash: code '+code);
 					server.close();
 				};
-				
+
 				phantom.on('exit',prematureExitHandler);
 			});
 		});
